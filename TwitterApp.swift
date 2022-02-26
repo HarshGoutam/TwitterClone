a80ca1c60f1ea95a64ac7ff0bdfe63ba362c43df
Harsh Goutam <87484626+HarshGoutam@users.noreply.github.com>
2022-02-26 23:55:50 +0530

    Add files via upload

---
+++ TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Harsh Goutam on 06/02/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
